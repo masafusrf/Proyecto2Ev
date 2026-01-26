3577d0a7d02f53494513b248b381d67ddfab048d
masafu <masafu@alumnatflorida.es>
2026-01-26 10:08:52 +0100

    creo clase madre

--- models/EntidadEstelar.php
+++ models/EntidadEstelar.php
@@ -1,7 +1,45 @@
 <?php
 
     abstract class EntidadEstelar{
-        
+        protected $id;
+        protected $nombre;
+        protected $planetaOrigen;
+        protected $estabilidad;
+
+        public function __construct($cod, $name, $planeta, $estabilidad){
+            $this->id = $cod;
+            $this->nombre=$name;
+            $this->planetaOrigen= $planeta;
+            $this->estabilidad=$estabilidad;
+        }
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+
+        public function setNombre($nombre){
+            $this->nombre=$nombre;
+        }
+
+        public function getPlaneta(){
+            return $this->planetaOrigen;
+        }
+
+        public function setPlaneta($planetaOrigen){
+            $this->planetaOrigen=$planetaOrigen;
+        }
+
+        public function getEstabilidad(){
+            return $this->estabilidad=$estabilidad;
+        }
+
+        public function setEstabilidad($estabilidad){
+            $this->estabilidad=$estabilidad;
+        }
     }
 
 ?>
